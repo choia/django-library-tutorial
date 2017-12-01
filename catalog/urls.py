162b5e694d5eb938af8aac693fbd6e750e3244da
Alex Choi <alex.ji.choi@gmail.com>
2017-12-01 14:14:57 -0500

    static & templates created, view functionality added

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^catalog/$', views.index),
+    url(r'^$', views.index, name='index'),
+    url(r'^authors/$', views.authors, name='authors'),
+    url(r'^books/$', views.books, name='books'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
+from .models import Book, BookInstance, Author, Genre 
+
 
 def index(request):
-	render(request, index.html)
+	num_books 		= Book.objects.all().count()
+	num_instances 	= BookInstance.objects.all().count()
+
+	num_instances_available 	= BookInstance.objects.filter(status__exact='a').count()
+	num_authors 				= Author.objects.all().count()
+
+	context = {
+		'num_books': num_books,
+		'num_instances': num_instances,
+		'num_instances_available': num_instances_available,
+		'num_authors': num_authors,
+	}
+
+	return render(request, 'index.html', context)
+
+
+def authors(request):
+	pass
+
+
+def books(request):
+	pass
